3d8a714eaf141ff37b821304ca0c747c8eb1d239
hilmanshini <hilmansphinx@gmail.com>
2019-07-14 23:25:23 +0700

    Create ConvertSettings.java

---
+++ ConvertSettings.java
@@ -0,0 +1,11 @@
+public class ConvertSettings {
+    String dateFormat = "yyyy-MM-dd";
+
+    public String getDateFormat() {
+        return dateFormat;
+    }
+
+    public void setDateFormat(String dateFormat) {
+        this.dateFormat = dateFormat;
+    }
+}
